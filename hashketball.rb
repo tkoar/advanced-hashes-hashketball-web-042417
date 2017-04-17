feff3da0633464040a410dfbeaf487ab56864305
github username <github email address>
2017-04-17 16:32:52 -0400

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,174 @@
 # Write your code here!
+require "pry"
 
+def game_hash
+  game_hash = {
+  :home => {
+    team_name: "Brooklyn Nets",
+    colors: ["Black", "White"],
+    players: [
+      {
+        player_name: "Alan Anderson",
+        number: 0,
+        shoe: 16,
+        points: 22,
+        rebounds: 12,
+        assists: 12,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 1
+      }, {
+        player_name: "Reggie Evans",
+        number: 30,
+        shoe: 14,
+        points: 12,
+        rebounds: 12,
+        assists: 12,
+        steals: 12,
+        blocks: 12,
+        slam_dunks: 7
+      }, {
+        player_name: "Brook Lopez",
+        number: 11,
+        shoe: 17,
+        points: 17,
+        rebounds: 19,
+        assists: 10,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 15
+      }, {
+        player_name: "Mason Plumlee",
+        number: 1,
+        shoe: 19,
+        points: 26,
+        rebounds: 12,
+        assists: 6,
+        steals: 3,
+        blocks: 8,
+        slam_dunks: 5
+      }, {
+        player_name: "Jason Terry",
+        number: 31,
+        shoe: 15,
+        points: 19,
+        rebounds: 2,
+        assists: 2,
+        steals: 4,
+        blocks: 11,
+        slam_dunks: 1
+      }
+    ]
+  },
+  :away => {
+    :team_name => "Charlotte Hornets",
+    :colors => ["Turquoise", "Purple"],
+    players: [
+      {
+        player_name: "Jeff Adrien",
+        number: 4,
+        shoe: 18,
+        points: 10,
+        rebounds: 1,
+        assists: 1,
+        steals: 2,
+        blocks: 7,
+        slam_dunks: 2
+      }, {
+        player_name: "Bismak Biyombo",
+        number: 0,
+        shoe: 16,
+        points: 12,
+        rebounds: 4,
+        assists: 7,
+        steals: 7,
+        blocks: 15,
+        slam_dunks: 10
+      }, {
+        player_name: "DeSagna Diop",
+        number: 2,
+        shoe: 14,
+        points: 24,
+        rebounds: 12,
+        assists: 12,
+        steals: 4,
+        blocks: 5,
+        slam_dunks: 5
+      }, {
+        player_name: "Ben Gordon",
+        number: 8,
+        shoe: 15,
+        points: 33,
+        rebounds: 3,
+        assists: 2,
+        steals: 1,
+        blocks: 1,
+        slam_dunks: 0
+      }, {
+        player_name: "Brendan Haywood",
+        number: 33,
+        shoe: 15,
+        points: 6,
+        rebounds: 12,
+        assists: 12,
+        steals: 22,
+        blocks: 5,
+        slam_dunks: 12
+      }
+    ]
+  }
+}
+end
 
+def players
+  all_players = (game_hash[:home][:players]) + (game_hash[:away][:players])
+end
 
+def teams
+  game_hash.values
+end
 
+def find_team(team_name)
+  teams.find { |team| team[:team_name] == team_name}
+end
 
+def find_player(name)
+  players.find {|player| player[:player_name] == name}
+end
 
+def num_points_scored(name)
+  player = find_player(name)
+  player[:points]
+end
 
+def shoe_size(name)
+  player = find_player(name)
+  player[:shoe]
+end
 
+def team_colors(team_name)
+  colors = find_team(team_name)
+  colors[:colors]
+end
 
+def team_names
+  game_hash.values.map do |team|
+    team[:team_name]
+  end
+end
 
+def player_numbers(team)
+  find_team(team)[:players].map do |player|
+    player[:number]
+  end
+end
+
+def player_stats(name)
+  players.find {|player| player[:player_name] == name}.reject { |key, value| key == :player_name }
+end
+
+def big_shoe_rebounds
+  player = players.sort_by do |player|
+    player[:shoe]
+  end.last[:rebounds]
+end
